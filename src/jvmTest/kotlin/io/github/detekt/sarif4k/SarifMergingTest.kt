e16c6fe1b1bf8b3d61a5708d0053eb206fc5fa27
Matej Drobnič <services+github@matejdro.com>
2024-02-09 23:47:49 +0100

    Add merging utilities (#87)
    * Add merging utilities
    
    * Add JvmName to Merging.kt
    
    Co-authored-by: Róbert Papp <papp.robert.s@gmail.com>
    
    * Switch to getResourceAsStream
    
    * Compare parsed sarif instead of raw text
    
    this should make it more reliable and ignore whitespace.
    
    * Attempt to fix JvmName on native
    
    Co-authored-by: Róbert Papp <papp.robert.s@gmail.com>
    
    * Match simpler tests style
    
    ---------
    
    Co-authored-by: Róbert Papp <papp.robert.s@gmail.com>

---
+++ src/jvmTest/kotlin/io/github/detekt/sarif4k/SarifMergingTest.kt
@@ -0,0 +1,36 @@
+package io.github.detekt.sarif4k
+
+import io.github.detekt.sarif4k.util.resourceAsTextContent
+import org.junit.jupiter.api.Test
+import java.io.File
+import kotlin.test.assertEquals
+
+class SarifMergingTest {
+    @Test
+    fun `merge output from sarifs of the same tool`() {
+        testMerge(
+            "output.sarif.json",
+            "input_1.sarif.json",
+            "input_2.sarif.json"
+        )
+    }
+
+    @Test
+    fun `merge output from sarifs with different tools`() {
+        testMerge(
+            "output_different_tool.sarif.json",
+            "input_1.sarif.json",
+            "input_2_different_tool.sarif.json"
+        )
+    }
+
+    private fun testMerge(expectedOutputFile: String, vararg inputFiles: String) {
+        val inputs = inputFiles.map {
+            SarifSerializer.fromJson(resourceAsTextContent(it))
+        }
+
+        val actual = inputs.reduce(SarifSchema210::merge)
+        val expected = SarifSerializer.fromJson(resourceAsTextContent(expectedOutputFile))
+        assertEquals(expected, actual)
+    }
+}
